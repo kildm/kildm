8f19cb838f4f7f6023538be0cfc83ee4b384f2ed
kildm <kildm@mail.ru>
2019-08-18 01:03:35 +0300

    add comment

--- conver_ipmask.py
+++ conver_ipmask.py
@@ -8,22 +8,23 @@ ip, mask = input().split('/')
 
 print('\n'+'-'*30)
 
-ip = ip.split('.')
+ip = ip.split('.') #Делим строку по точке
 #print(type(mask))
 
 
-ip = [int(item) for item in ip]
-mask = int(mask)
-maskbin = (mask*'1'+('0'*(32-mask)))
+ip = [int(item) for item in ip] # Переводим в список
+mask = int(mask) # Переводим в число
+maskbin = (mask*'1'+('0'*(32-mask)))# получаем 32 битную строку маски
 formats = '''
 	 {0:<8} {1:<8} {2:<8} {3:<8}
 	 {0:<08b} {1:<08b} {2:<08b} {3:<08b}
 '''
+#создаем шаблон
 
 
-binlist = [maskbin[0:8],maskbin[8:16],maskbin[16:24],maskbin[24:32]]
+binlist = [maskbin[0:8],maskbin[8:16],maskbin[16:24],maskbin[24:32]] #получаем список октетов маски 
 
-binlist = [int(item,2) for item in binlist]
+binlist = [int(item,2) for item in binlist] # Переводим строки в числа
 
 
 
